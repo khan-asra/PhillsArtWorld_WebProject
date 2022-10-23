0d93d10e35ee66e3a848844a962dbc7f9a4d2a9b
khan-asra <khanasr@sheridancollege.ca>
2022-10-22 21:25:05 -0400

    configuration setting done
    allowed the /sendMail

--- src/main/java/ca/sheridancollege/khanasr/configuration/WebSecurityConfiguration.java
+++ src/main/java/ca/sheridancollege/khanasr/configuration/WebSecurityConfiguration.java
@@ -59,7 +59,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors();
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("*/*", "/submitContactEmail","/getAllFaq","/authenticate","/registerNewUser","/getGallery","/galleryProducts","/getAllProducts").permitAll()
+                .authorizeRequests().antMatchers("*/*", "/submitContactEmail","/getAllFaq","/authenticate","/registerNewUser","/getGallery","/galleryProducts","/getAllProducts","/sendMail").permitAll()
                 .antMatchers(HttpHeaders.ALLOW).permitAll()
                 .anyRequest().authenticated()
                 .and()
